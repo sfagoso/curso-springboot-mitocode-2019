8d7d33aa01712e37341fbabe5aabb9ef51db40ff
saul <ing.saulfragoso@gmail.com>
2020-09-21 22:52:04 -0500

    Cambios para Spring Security

--- src/test/java/com/mitocode/DemoWebApplicationTests.java
+++ src/test/java/com/mitocode/DemoWebApplicationTests.java
@@ -1,13 +1,33 @@
 package com.mitocode;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mitocode.model.Usuario;
+import com.mitocode.repo.IUsuarioRepo;
 
 @SpringBootTest
 class DemoWebApplicationTests {
 
+	@Autowired	
+	private IUsuarioRepo repo;
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Test
-	void contextLoads() {
+	void crearUsuar() {
+		Usuario us = new Usuario();
+		us.setId(4);
+		us.setNombre("codex");
+		us.setClave(encoder.encode("123"));
+		Usuario retorno = repo.save(us);
+		
+		assertTrue(retorno.getClave().equalsIgnoreCase(us.getClave()));
 	}
 
 }
