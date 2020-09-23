a8ccd0375357d31402ec9ac5aa8619179c0a1d3a
saul <ing.saulfragoso@gmail.com>
2020-09-23 18:03:37 -0500

    cambios a postgresql

--- src/main/java/com/mitocode/controller/DemoController.java
+++ src/main/java/com/mitocode/controller/DemoController.java
@@ -20,8 +20,8 @@ public class DemoController {
 		//logica
 		
 		Persona p = new Persona();
-		p.setIdPersona(1);
-		p.setNombre("MitoCode");
+		p.setIdPersona(2);
+		p.setNombre("Code");
 		repo.save(p);
 		
 		model.addAttribute("name", name);
